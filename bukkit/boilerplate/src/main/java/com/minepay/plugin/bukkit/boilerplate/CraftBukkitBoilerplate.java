ffb07fe94220dd61b0fec34e92126022f704fa7e
Johannes Donath <johannesd@torchmind.com>
2016-07-10 13:10:29 +0200

    Added a boilerplate in order to handle NMS compatibility properly.

---
+++ bukkit/boilerplate/src/main/java/com/minepay/plugin/bukkit/boilerplate/CraftBukkitBoilerplate.java
@@ -0,0 +1,45 @@
+package com.minepay.plugin.bukkit.boilerplate;
+
+import org.bukkit.Bukkit;
+
+import java.util.Optional;
+
+import javax.annotation.Nonnegative;
+import javax.annotation.Nonnull;
+
+/**
+ * Provides direct access to NMS in order to simplify or otherwise optimize processes.
+ *
+ * @author <a href="mailto:johannesd@torchmind.com">Johannes Donath</a>
+ */
+public interface CraftBukkitBoilerplate {
+
+    /**
+     * Retrieves a boilerplate instance depending on the reported server version.
+     *
+     * @return a boilerplate implementation or, if no matching implementation could be located, an
+     * empty optional.
+     */
+    @Nonnull
+    static Optional<CraftBukkitBoilerplate> getInstance() {
+        String serverVersion = Bukkit.getServer().getVersion().replace('.', '_');
+
+        try {
+            Class<?> clazz = Class.forName("com.minepay.plugin.bukkit.boilerplate." + serverVersion + ".CraftBukkitBoilerplateImpl");
+            return Optional.of(clazz.asSubclass(CraftBukkitBoilerplate.class).newInstance());
+        } catch (ClassNotFoundException ignore) {
+        } catch (IllegalAccessException | InstantiationException ex) {
+            throw new RuntimeException("Could not instantiate CraftBukkit support implementation from com.minepay.plugin.bukkit.boilerplate." + serverVersion + ".CraftBukkitBoilerplateImpl: " + ex.getMessage(), ex);
+        }
+
+        return Optional.empty();
+    }
+
+    /**
+     * Retrieves the overall amount of ticks processed by the server since startup.
+     *
+     * @return an amount of ticks.
+     */
+    @Nonnegative
+    int getTickCount();
+}

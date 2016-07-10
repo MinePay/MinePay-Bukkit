edf7498bdb80ce6c7e0eed571bc90bac4269933e
Johannes Donath <johannesd@torchmind.com>
2016-07-10 13:38:26 +0200

    Added an actual implementation.

--- bukkit/boilerplate-outdated/src/main/java/net/minepay/plugin/bukkit/boilerplate/OutdatedBukkitBoilerplate.java
+++ bukkit/boilerplate-outdated/src/main/java/net/minepay/plugin/bukkit/boilerplate/OutdatedBukkitBoilerplate.java
@@ -1,19 +1,29 @@
 package net.minepay.plugin.bukkit.boilerplate;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
+import java.util.Arrays;
 import java.util.Collection;
 
 import javax.annotation.Nonnull;
 
 /**
+ * Provides a boilerplate for older Bukkit versions in order to work around changes to the API.
+ *
  * @author <a href="mailto:johannesd@torchmind.com">Johannes Donath</a>
  */
 public class OutdatedBukkitBoilerplate implements BukkitBoilerplate {
 
+    /**
+     * {@inheritDoc}
+     */
     @Nonnull
     @Override
     public Collection<? extends Player> getOnlinePlayers() {
-        return null;
+        // A view would probably be a better way of handling this, however the reconstruction of
+        // this array in Bukkit itself would break this concept majorly ... which is probably why
+        // this method was deprecated in newer Bukkit versions
+        return Arrays.asList(Bukkit.getOnlinePlayers());
     }
 }

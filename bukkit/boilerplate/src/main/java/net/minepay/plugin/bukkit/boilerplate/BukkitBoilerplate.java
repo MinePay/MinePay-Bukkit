8617f3588109c1b7352df97fc3834982b5862bb9
Johannes Donath <johannesd@torchmind.com>
2016-07-10 14:03:43 +0200

    Added a method of retrieving the correct boilerplate implementation.

--- bukkit/boilerplate/src/main/java/net/minepay/plugin/bukkit/boilerplate/BukkitBoilerplate.java
+++ bukkit/boilerplate/src/main/java/net/minepay/plugin/bukkit/boilerplate/BukkitBoilerplate.java
@@ -1,8 +1,11 @@
 package net.minepay.plugin.bukkit.boilerplate;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import java.util.Collection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.annotation.Nonnull;
 
@@ -13,6 +16,36 @@ import javax.annotation.Nonnull;
  * @author <a href="mailto:johannesd@torchmind.com">Johannes Donath</a>
  */
 public interface BukkitBoilerplate {
+    Pattern VERSION_PATTERN = Pattern.compile("^(\\d+)\\.(\\d+)\\.(\\d+)(.*)$");
+
+    /**
+     * Retrieves a Bukkit boilerplate instance which is designed to wrap the current server API
+     * version.
+     *
+     * @return a boilerplate implementation.
+     */
+    @Nonnull
+    static BukkitBoilerplate getInstance() {
+        Matcher matcher = VERSION_PATTERN.matcher(Bukkit.getVersion());
+
+        if (matcher.matches()) {
+            return new ModernBukkitBoilerplate();
+        }
+
+        try {
+            int major = Integer.parseUnsignedInt(matcher.group(1));
+            int minor = Integer.parseUnsignedInt(matcher.group(2));
+
+            if (major <= 1 && minor <= 7) {
+                return Class.forName("net.minepay.plugin.bukkit.boilerplate").asSubclass(BukkitBoilerplate.class).newInstance();
+            }
+        } catch (NumberFormatException ignore) {
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
+            throw new RuntimeException("Could not load compatibility layer: " + ex.getMessage(), ex);
+        }
+
+        return new ModernBukkitBoilerplate();
+    }
 
     /**
      * Retrieves a collection of players which are currently connected to the server.

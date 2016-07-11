1f12cfaf4c7e90a0b67dbd9a381337787bc135e2
Johannes Donath <johannesd@torchmind.com>
2016-07-11 15:55:37 +0200

    Switched methods of version detection to follow the CraftBukkit scheme.

--- bukkit/boilerplate/src/main/java/net/minepay/plugin/bukkit/boilerplate/CraftBukkitBoilerplate.java
+++ bukkit/boilerplate/src/main/java/net/minepay/plugin/bukkit/boilerplate/CraftBukkitBoilerplate.java
@@ -22,7 +22,7 @@ public interface CraftBukkitBoilerplate {
      */
     @Nonnull
     static Optional<CraftBukkitBoilerplate> getInstance() {
-        String serverVersion = Bukkit.getServer().getVersion().replace('.', '_');
+        String serverVersion = Bukkit.getServer().getClass().getPackage().getName().replaceAll("^.*\\.v(\\d+)_(\\d+)_R(\\d+)\\..*$", "v$1_$2_R$3");
 
         try {
             Class<?> clazz = Class.forName("com.minepay.plugin.bukkit.boilerplate." + serverVersion + ".CraftBukkitBoilerplateImpl");

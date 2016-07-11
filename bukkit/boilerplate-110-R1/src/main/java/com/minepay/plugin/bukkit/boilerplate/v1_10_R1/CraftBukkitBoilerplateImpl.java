6aa44d2600d36a2f83d5db83f82a63c58ccf3bbe
Johannes Donath <johannesd@torchmind.com>
2016-07-11 16:47:23 +0200

    Added a boilerplate implementation for 1.10 (R1).

---
+++ bukkit/boilerplate-110-R1/src/main/java/com/minepay/plugin/bukkit/boilerplate/v1_10_R1/CraftBukkitBoilerplateImpl.java
@@ -0,0 +1,36 @@
+package com.minepay.plugin.bukkit.boilerplate.v1_10_R1;
+
+import net.minecraft.server.v1_10_R1.MinecraftServer;
+import net.minepay.plugin.bukkit.boilerplate.CraftBukkitBoilerplate;
+
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_10_R1.CraftServer;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+
+/**
+ * Provides a CraftBukkit boilerplate which is capable of wrapping calls to NMS for a certain server
+ * version.
+ *
+ * @author <a href="mailto:johannesd@torchmind.com">Johannes Donath</a>
+ */
+public class CraftBukkitBoilerplateImpl implements CraftBukkitBoilerplate {
+    private final MethodHandle handle;
+
+    public CraftBukkitBoilerplateImpl() throws NoSuchFieldException, IllegalAccessException {
+        this.handle = MethodHandles.publicLookup().findGetter(MinecraftServer.class, "tick", Integer.class);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getTickCount() {
+        try {
+            return (int) this.handle.invoke(((CraftServer) Bukkit.getServer()).getHandle());
+        } catch (Throwable ex) {
+            throw new RuntimeException("Could not access NMS tick count: " + ex.getMessage(), ex);
+        }
+    }
+}

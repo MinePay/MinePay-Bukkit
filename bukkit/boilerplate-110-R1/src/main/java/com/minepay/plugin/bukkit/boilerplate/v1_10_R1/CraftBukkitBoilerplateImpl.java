8b3fc78a2d9d19fc639e0ee383762f1eb5c02cd3
Johannes Donath <johannesd@torchmind.com>
2016-07-12 17:08:05 +0200

    Fixed access to the internal tick counter (find the CraftBukkit stupidity and win a virtual cookie).

--- bukkit/boilerplate-110-R1/src/main/java/com/minepay/plugin/bukkit/boilerplate/v1_10_R1/CraftBukkitBoilerplateImpl.java
+++ bukkit/boilerplate-110-R1/src/main/java/com/minepay/plugin/bukkit/boilerplate/v1_10_R1/CraftBukkitBoilerplateImpl.java
@@ -1,13 +1,15 @@
 package com.minepay.plugin.bukkit.boilerplate.v1_10_R1;
 
-import net.minecraft.server.v1_10_R1.MinecraftServer;
 import com.minepay.plugin.bukkit.boilerplate.CraftBukkitBoilerplate;
+
+import net.minecraft.server.v1_10_R1.MinecraftServer;
 
 import org.bukkit.Bukkit;
 import org.bukkit.craftbukkit.v1_10_R1.CraftServer;
 
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
+import java.lang.reflect.Field;
 
 /**
  * Provides a CraftBukkit boilerplate which is capable of wrapping calls to NMS for a certain server
@@ -19,7 +21,10 @@ public class CraftBukkitBoilerplateImpl implements CraftBukkitBoilerplate {
     private final MethodHandle handle;
 
     public CraftBukkitBoilerplateImpl() throws NoSuchFieldException, IllegalAccessException {
-        this.handle = MethodHandles.publicLookup().findGetter(MinecraftServer.class, "tick", Integer.class);
+        Field field = MinecraftServer.class.getDeclaredField("ticks");
+        field.setAccessible(true);
+
+        this.handle = MethodHandles.lookup().unreflectGetter(field);
     }
 
     /**
@@ -28,7 +33,7 @@ public class CraftBukkitBoilerplateImpl implements CraftBukkitBoilerplate {
     @Override
     public int getTickCount() {
         try {
-            return (int) this.handle.invoke(((CraftServer) Bukkit.getServer()).getHandle());
+            return (int) this.handle.invoke(((CraftServer) Bukkit.getServer()).getServer());
         } catch (Throwable ex) {
             throw new RuntimeException("Could not access NMS tick count: " + ex.getMessage(), ex);
         }

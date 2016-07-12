6db257182519e0d2b6d953d987edfeb8d71dda1c
Johannes Donath <johannesd@torchmind.com>
2016-07-12 09:48:18 +0200

    Added a runnable which will be used in place of the NMS backend when no support for the current server version has been implemented yet.

---
+++ bukkit/core/src/main/java/net/minepay/plugin/bukkit/task/TickCounterTask.java
@@ -0,0 +1,32 @@
+package net.minepay.plugin.bukkit.task;
+
+import javax.annotation.Nonnegative;
+
+/**
+ * Keeps track of all ticks that have been processed by this server since the plugin has been
+ * loaded.
+ *
+ * @author <a href="mailto:johannesd@torchmind.com">Johannes Donath</a>
+ */
+public class TickCounterTask implements Runnable {
+    private int counter;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void run() {
+        ++this.counter;
+    }
+
+    /**
+     * Retrieves the overall amount of ticks processed by this server since this plugin has been
+     * loaded.
+     *
+     * @return an amount of ticks.
+     */
+    @Nonnegative
+    public int getTickCount() {
+        return this.counter;
+    }
+}
